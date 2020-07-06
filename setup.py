e31f149af05fafe9edbcc124f53585b611c68078
danieleperera <daniele.storage1@gmail.com>
2020-07-06 22:45:06 +0000

    omitted emails on README

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def readme_file_contents():
 
 
 setup(
-    name='OnionScraper',
+    name='OnionIngestor',
     version='1.0.0',
     description='Python app to scraper and index hidden websites',
     long_description=readme_file_contents(),
     author='dan',
     author_email='test@google.com',
     license='MIT',
-    packages=['onionscraper'],
+    packages=['onioningestor'],
     zip_safe=False,
     install_requires=[]
 )
